fdae5bcb52b97e3c8112b8723ea48f3944fe93c7
Alex <alex@litepages.co>
2021-08-01 00:27:22 +1000

    wip

--- projects/frontend/nuxt.config.js
+++ projects/frontend/nuxt.config.js
@@ -1,4 +1,16 @@
 // import "7.css/dist/7.css";
+require('dotenv').config();
+
+
+const APP_URL = process.env.APP_URL || 'http://localhost:4000';
+const API_URL = process.env.API_URL || 'http://localhost:3000';
+
+if (!process.env.APP_URL || !process.env.API_URL) {
+    console.warn('No API_URL or APP_URL provided in environment, using defaults');
+  }
+  if (!process.env.NUXT_ENV) {
+    console.warn('No environments provided in environment, using defaults');
+  }
 
 export default {
     head: {
@@ -9,5 +21,7 @@ export default {
     css: [
         // Load a Node.js module directly (here it's a Sass file)
         "7.css/dist/7.css"
-    ]
+    ],
+    mode: "spa",
+    components: 'true'
 }
