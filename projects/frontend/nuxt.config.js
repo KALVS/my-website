5d09eb2fdd7704d80dff45b10da11f0bd3db4ae5
Alex <alex@litepages.co>
2021-08-01 11:40:37 +1000

    eslint going

--- projects/frontend/nuxt.config.js
+++ projects/frontend/nuxt.config.js
@@ -1,27 +1,27 @@
+/* eslint-disable no-console */
 // import "7.css/dist/7.css";
 require('dotenv').config();
 
-
-const APP_URL = process.env.APP_URL || 'http://localhost:4000';
-const API_URL = process.env.API_URL || 'http://localhost:3000';
+// const APP_URL = process.env.APP_URL || 'http://localhost:4000';
+// const API_URL = process.env.API_URL || 'http://localhost:3000';
 
 if (!process.env.APP_URL || !process.env.API_URL) {
-    console.warn('No API_URL or APP_URL provided in environment, using defaults');
-  }
-  if (!process.env.NUXT_ENV) {
-    console.warn('No environments provided in environment, using defaults');
-  }
+  console.warn('No API_URL or APP_URL provided in environment, using defaults');
+}
+if (!process.env.NUXT_ENV) {
+  console.warn('No environments provided in environment, using defaults');
+}
 
 export default {
-    head: {
-        title: 'my title',
-        meta: [
-          { charset: 'utf-8' }]
-    },
-    css: [
-        // Load a Node.js module directly (here it's a Sass file)
-        "7.css/dist/7.css"
-    ],
-    mode: "spa",
-    components: 'true'
-}
+  head: {
+    title: 'my title',
+    meta: [
+      { charset: 'utf-8' }],
+  },
+  css: [
+    // Load a Node.js module directly (here it's a Sass file)
+    '7.css/dist/7.css',
+  ],
+  mode: 'spa',
+  components: 'true',
+};
--- projects/frontend/pages/index.vue
+++ projects/frontend/pages/index.vue
@@ -7,11 +7,9 @@
 
 <style>
 
-
-
   body {
       background: black;
-      overflow: hidden;  
+      overflow: hidden;
       margin: 0;
       padding: 0;
     }
@@ -23,69 +21,65 @@
       padding: 0;
       z-index: -1;
     }
-         
+
 </style>
 
 <script>
 import home from '../components/home.vue';
-  export default {
-    components: { home },
-    mounted() {
-      // geting canvas by Boujjou Achraf
-        var c = document.getElementById("c");
-        var ctx = c.getContext("2d");
 
-        //making the canvas full screen
-        c.height = window.outerHeight;
-        c.width = window.outerWidth;
+export default {
+  components: { home },
+  mounted() {
+    // geting canvas by Boujjou Achraf
+    const c = document.getElementById('c');
+    const ctx = c.getContext('2d');
 
-        //chinese characters - taken from the unicode charset
-        var matrix = '多年来我会来谁会发挥她锻炼的优势让学区长生不老想成为一个痛苦的丘比特逃离产生没有结果的快乐他们抛弃了那些应该为你的麻烦负责的人的一般职责'
-        // var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
-        //converting the string into an array of single characters
-        matrix = matrix.split("");
+    // making the canvas full screen
+    c.height = window.outerHeight;
+    c.width = window.outerWidth;
 
-        var font_size = 10;
-        var columns = c.width/font_size; //number of columns for the rain
-        //an array of drops - one per column
-        var drops = [];
-        //x below is the x coordinate
-        //1 = y co-ordinate of the drop(same for every drop initially)
-        for(var x = 0; x < columns; x++)
-            drops[x] = 1; 
+    // chinese characters - taken from the unicode charset
+    let matrix = '多年来我会来谁会发挥她锻炼的优势让学区长生不老想成为一个痛苦的丘比特逃离产生没有结果的快乐他们抛弃了那些应该为你的麻烦负责的人的一般职责';
+    // eslint-disable-next-line max-len
+    // var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+    // converting the string into an array of single characters
+    matrix = matrix.split('');
 
-        //drawing the characters
-        function draw()
-        {
-            //Black BG for the canvas
-            //translucent BG to show trail
-            ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
-            ctx.fillRect(0, 0, c.width, c.height);
+    const fontSize = 10;
+    const columns = c.width / fontSize; // number of columns for the rain
+    // an array of drops - one per column
+    const drops = [];
+    // x below is the x coordinate
+    // 1 = y co-ordinate of the drop(same for every drop initially)
+    for (let x = 0; x < columns; x += 1) drops[x] = 1;
 
-            ctx.fillStyle = "#0f0";//green text
-            ctx.font = font_size + "px arial";
-            //looping over drops
-            for(var i = 0; i < drops.length; i++)
-            {
-                //a random chinese character to print
-                var text = matrix[Math.floor(Math.random()*matrix.length)];
-                //x = i*font_size, y = value of drops[i]*font_size
-                ctx.fillText(text, i*font_size, drops[i]*font_size);
+    // drawing the characters
+    function draw() {
+      // Black BG for the canvas
+      // translucent BG to show trail
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+      ctx.fillRect(0, 0, c.width, c.height);
 
-                //sending the drop back to the top randomly after it has crossed the screen
-                //adding a randomness to the reset to make the drops scattered on the Y axis
-                if(drops[i]*font_size > c.height && Math.random() > 0.975)
-                    drops[i] = 0;
+      ctx.fillStyle = '#0f0';// green text
+      ctx.font = `${fontSize}px arial`;
+      // looping over drops
+      for (let i = 0; i < drops.length; i += 1) {
+        // a random chinese character to print
+        const text = matrix[Math.floor(Math.random() * matrix.length)];
+        // x = i*fontSize, y = value of drops[i]*fontSize
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
-                //incrementing Y coordinate
-                drops[i]++;
-            }
-        }
+        // sending the drop back to the top randomly after it has crossed the screen
+        // adding a randomness to the reset to make the drops scattered on the Y axis
+        if (drops[i] * fontSize > c.height && Math.random() > 0.975) drops[i] = 0;
 
-        setInterval(draw, 35);
+        // incrementing Y coordinate
+        drops[i] += 1;
+      }
+    }
 
-
-    },
-  }
+    setInterval(draw, 35);
+  },
+};
 
 </script>
