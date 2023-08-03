5beff334373d6af568cc22e5f3a971ae5fa69408
Alex <a.holm57@hotmail.com>
2023-08-03 13:44:32 +1000

    n

--- frontend/plugins/stripe.js
+++
@@ -1,9 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe(process.env.STRIPE_TEST_SKEY);
-
-export default (context, inject) => {
-  stripePromise.then((stripe) => {
-    inject('stripe', stripe);
-  });
-};
