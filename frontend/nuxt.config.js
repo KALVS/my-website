88035dee3805998401c4633e5df8fa41da6acc41
Alex <a.holm57@hotmail.com>
2024-12-04 17:41:59 +1100

    simplify

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -36,12 +36,14 @@ export default {
     [
       'nuxt-stripe-module',
       {
-        publishableKey: process.env.STRIPE_PKEY
+        publishableKey:
+          'pk_live_51N7DUbA5NSCLB33rSNbmAlVqevYKtNj8tdu502XpAm9pOq0ZoyJaQ7BT4qH7JmauWEbhQgX3Yg88vk34RbD6qpLY005xDDBwjG'
       }
     ]
   ],
   stripe: {
-    publishableKey: process.env.STRIPE_PKEY,
+    publishableKey:
+      'pk_live_51N7DUbA5NSCLB33rSNbmAlVqevYKtNj8tdu502XpAm9pOq0ZoyJaQ7BT4qH7JmauWEbhQgX3Yg88vk34RbD6qpLY005xDDBwjG',
     apiVersion: '2020-08-27'
   },
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/eslint-module']
